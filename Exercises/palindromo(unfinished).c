7ed65a951db03325214ad3308245b1cd17cc6a04
Thiago Crispim <taraujocrispim@gmail.com>
2019-10-31 11:41:53 -0300

    Create palindromo(unfinished).c

---
+++ Exercises/palindromo(unfinished).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindromo (char str[201], int i, int j);
+
+int main() {
+ char str[201];
+ while (scanf ("%s", str) == 1) {
+ if (isPalindromo (str, 0, strlen(str) - 1)) {
+ printf ("sim\n");
+ }
+ else {
+ printf ("nao\n");
+ }
+ }
+ return 0;
+}
+
+bool isPalindromo (char str[201], int i, int j){
+ 
+ for(i<0;i<=j;i++){
+   if (str[i]=='E'){
+     if(str[i+1]=='O'){
+       if(str[i+2]=='F') return 0;
+     }
+   }
+ }
+
+}
