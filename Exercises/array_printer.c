f49ad7d744c4fcbe5393f485f49a39d112860fd0
Thiago Crispim <taraujocrispim@gmail.com>
2019-10-31 09:24:38 -0300

    Update array_printer.c
    Added some more information about the function

--- Exercises/array_printer.c
+++ Exercises/array_printer.c
@@ -35,3 +35,46 @@ void array_Printer(int n, int vec[]){
   }
   printf("%d}\n",vec[n-1]);
 }
+
+//Below, there is an example of a compressed function I mentioned in the beginning 
+
+/*#include <stdio.h>
+
+//Be careful with global variables
+int g_n;
+
+void array_Reader(int n, int vec[]);
+void array_Printer(int n, int vec[]);
+void Read_n_Print(int n, int vec[]);
+
+int main(){
+  printf("Type n\n");
+  scanf("%d",&g_n);
+  int array1[g_n];
+  Read_n_Print(g_n, array1);
+  return 0;
+}
+
+void array_Reader(int n, int vec[]){
+  for(int i = 0; i<n; i++){
+    printf("There are %d empty spaces remaining\n",n-i);
+    scanf("%d\n",&vec[i]);
+  }
+}
+
+void array_Printer(int n, int vec[]){
+  printf("The array stored is\n{");
+  for(int i = 0; i<n-1; i++){
+    printf("%d,",vec[i]);
+  }
+  printf("%d}\n",vec[n-1]);
+}
+
+void Read_n_Print(int n, int vec[]){
+  //printf("Type n\n");
+  //scanf("%d",&n);
+  //Declaring the array
+  int ar1[n];
+  array_Reader(n,ar1);
+  array_Printer(n,ar1);
+}*/
